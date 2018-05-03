2e2b5d574624498aea3e9ec26a3f5b0034a4cc58
Alex <alexsmekhunov@gmail.com>
2018-05-03 03:16:40 +0300

    added DB

--- DB.h
+++ DB.h
@@ -6,9 +6,72 @@
 
 using namespace std;
 
+struct Tags
+{
+    string chat_id;
+    int sz;
+    string* tags;
+    Tags(){sz=0;}
+    ~Tags(){}
+};
+
+struct Selection
+{
+    int size;
+    Tags* subs;
+    Selection(){size=0;subs=nullptr;}
+    ~Selection(){}
+};
+
+Selection DB;
+
 void add_tag(const string &chat_id,const string &tag)
 {
+    int i=0,j=0;
+    for(i=0;i<DB.size;++i)
+    {
+        if(DB.subs[i].chat_id==chat_id)
+        {
+            for(j=0;j<DB.subs[i].sz;++j)
+            {
+                if(DB.subs[i].tags[j]==tag)
+                {
+                    return;
+                }
+            }
+            break;
+        }
+    }
+    if(i==DB.size)
+    {
+        Tags* new_subs = new Tags[++(DB.size)];
+        for(int k=0;k<DB.size-1;++k)
+        {
+            new_subs[k]=DB.subs[k];
+        }
+        new_subs[DB.size-1].chat_id=chat_id;
+        new_subs[DB.size-1].sz=1;
+        new_subs[DB.size-1].tags=new string[new_subs[DB.size-1].sz];
+        new_subs[DB.size-1].tags[0]=tag;
+        if(DB.subs!=nullptr)
+            delete[] DB.subs;
+        DB.subs=new_subs;
+        return;
+    }
+    if(j==DB.subs[i].sz)
+    {
+        ++(DB.subs[i].sz);
+        string *new_tags = new string[DB.subs[i].sz];
+        for (int k = 0; k < DB.subs[i].sz-1; ++k)
+        {
+            new_tags[k] = DB.subs[i].tags[k];
+        }
+        new_tags[DB.subs[i].sz-1]=tag;
+        delete[] DB.subs[i].tags;
+        DB.subs[i].tags = new_tags;
 
+        return;
+    }
 }
 
 void delete_tag(const string &chat_id,const string &tag)
@@ -26,9 +89,9 @@ string get_tags_by_subscriber(const string &chat_id)
     return string("nil");
 }
 
-string** get_all_subscribers()
+Selection* get_all_subscribers()
 {
-
+    return &DB;
 }
 
 #endif
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/grisha/Git/JobBot
+# Install script for directory: /home/alex/TP-MAIL/CPP1/BOT/JobBot
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/grisha/Git/JobBot/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/alex/TP-MAIL/CPP1/BOT/JobBot/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

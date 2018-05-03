d4a2c5bc2ee3badc3dad65bfaab85ccfc5e7a085
Яна <yana@MacBook-Pro-Ana.local>
2018-05-03 20:30:41 +0300

    func db

--- DB.h
+++ DB.h
@@ -3,32 +3,129 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <sstream>
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
 
 using namespace std;
 
-void add_tag(const string &chat_id,const string &tag)
+
+/* Создание подключения с БД */
+std::auto_ptr <sql::Connection> create_connection()
+{
+
+    /* Create a connection */
+    sql::Driver* driver = get_driver_instance();
+    std::auto_ptr <sql::Connection> con(driver->connect("tcp://127.0.0.1:3306", "adminbot", "botjobdb1234"));
+    /* Connect to the MySQL database */
+    con->setSchema("botjob");
+
+    return std::move(con);
+
+}
+
+/* Нахождение строки по PK
+ * в mySQL также создана хранимая процедура,
+ * выполняющая SELECT по таблице */
+std::string find_row(int chatID)
+{
+
+    std::string query;
+    std::string id = std::to_string(chatID);
+
+    std::auto_ptr <sql::Connection> con = create_connection();
+    std::auto_ptr <sql::Statement> stmt(con->createStatement());
+
+    query = "CALL getWords( " + id + ", @pop)";
+
+    stmt->execute(query);
+    std::auto_ptr <sql::ResultSet> res(stmt->executeQuery("SELECT @pop AS _reply"));
+
+    res->next();
+    con->close();
+    return res->getString("_reply");
+}
+
+void add_tag(int chatID, const std::string& data)
+{
+    std::auto_ptr <sql::Connection> con = create_connection();
+    std::auto_ptr <sql::Statement> stmt(con->createStatement());
+
+    std::string query;
+
+    query = "CALL addRow(NULL,'" + data + "' )";
+    stmt->execute(query);
+    con->close();
+
+}
+
+void delete_tag(string chat_id, string tag)
 {
 
 }
 
-void delete_tag(const string &chat_id,const string &tag)
+void delete_all_tags(int chatID)
 {
+    std::auto_ptr <sql::Connection> con = create_connection();
+    std::auto_ptr <sql::Statement> stmt(con->createStatement());
 
+    std::string query;
+    std::string id = std::to_string(chatID);
+
+    query = "CALL deleteRow(" + id + ")";
+    stmt->execute(query);
+    con->close();
 }
 
-void delete_all_tags(const string &chat_id)
-{
-
-}
-
-string get_tags_by_subscriber(const string &chat_id)
+string get_tags_by_subscriber(string chat_id)
 {
     return string("nil");
 }
 
-string** get_all_subscribers()
+
+/* Изменение строки в БД
+ * На вход необходимо подавать РК - id чата, а также
+ * данные, которые необходимо ДОБАВИТЬ к существующим */
+void update_row(int chatID, const std::string& data)
 {
 
+    std::auto_ptr <sql::Connection> con = create_connection();
+    std::auto_ptr <sql::Statement> stmt(con->createStatement());
+
+    std::string query;
+    std::string id = std::to_string(chatID);
+
+    query = "CALL updateRow('" + id + "', '" + data + "')";
+    stmt->execute(query);
+    con->close();
+}
+
+std::string** get_all_subscribers() {
+    std::auto_ptr<sql::Connection> con = create_connection();
+    std::auto_ptr<sql::Statement> stmt(con->createStatement());
+    int key = 0;
+
+    stmt->execute("CALL get_data()");
+    std::auto_ptr< sql::ResultSet > res;
+    std::string** result = new string[256];
+    do {
+        res.reset(stmt->getResultSet());
+        while (res->next()) {
+            std::cout << "chat: " << (std::string)res->getString("chatID") << " key: " << (std::string)res->getString("keyWords")
+                      << std::endl;
+            result[key][0] = (std::string)res->getString("keyWords");
+            key++;
+        }
+    } while (stmt->getMoreResults());
+
+    con->close();
+    return result;
 }
 
 #endif
---
+++ sql_func.sql
@@ -0,0 +1,26 @@
+DELIMITER $$
+
+CREATE PROCEDURE getWords ( IN id int, out words char(255) )
+    BEGIN
+     SELECT keyWords INTO words FROM chat
+     WHERE chatID=id;
+    END
+$$
+
+CREATE PROCEDURE addRow( IN id int, IN data char(255) )
+    BEGIN
+        INSERT INTO chat (chatID, keyWords) VALUES(NULL, data);
+    END
+$$
+
+CREATE PROCEDURE deleteRow( IN id int )
+    BEGIN
+       DELETE from chat where chatID=id;
+    END$$
+
+CREATE PROCEDURE updateRow( IN id INT, IN data CHAR(255) )
+    BEGIN
+        UPDATE chat
+        SET keyWords = CONCAT(keyWords, ' ', data)
+        WHERE chatID = id;
+    END $$

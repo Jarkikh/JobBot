646904d1ac05ba56307e40d5fb31dc2b78fcfef7
Alex <alexsmekhunov@gmail.com>
2018-05-03 03:23:34 +0300

    added everything

--- Telegram.h
+++ Telegram.h
@@ -5,15 +5,15 @@
 #ifndef JOBBOT_TELEGRAM_H
 #define JOBBOT_TELEGRAM_H
 
-#endif //JOBBOT_TELEGRAM_H
 
 #include <iostream>
 
-using namespace std;
 
-struct Message{
-    string chat_id;
-    string text;
+struct Message
+{
+    std::string chat_id;
+    std::string text;
+    std::string first_name;
 
     Message(){
 
@@ -23,6 +23,20 @@ struct Message{
     }
 };
 
+
+
 void send_message(string message, string chat_id){
-    // используем curl
+    std::string result=std::string("https://api.telegram.org/bot524193239:AAEn0Ovfo7jX3QXIYH19kIHlfNHEx1EBr2Y/sendMessage?chat_id=")+chat_id+std::string("&text=")+message;
+    std::cout << result;
+    CURL * curl_handle = curl_easy_init();
+    if(curl_handle)
+    {
+        curl_easy_setopt(curl_handle, CURLOPT_URL, result.c_str());
+        curl_easy_setopt(curl_handle, CURLOPT_PROXY,"https://147.135.210.114:54566");
+        //curl_easy_setopt(curl_handle, CURLOPT_VERBOSE,1);
+        curl_easy_perform(curl_handle);
+        curl_easy_cleanup(curl_handle);
+    }
 }
+
+#endif //JOBBOT_TELEGRAM_H
